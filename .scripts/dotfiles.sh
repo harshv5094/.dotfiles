87a607f69efbe2858dc8260f7c9c4eb9f54fabc4
harshv5094 (Local User) <harshv5094@gmail.com>
2023-12-08 12:11:35 +0530

    feat(dotfiles.sh): New script for linking repo's dotfiles with system

---
+++ .scripts/dotfiles.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+ln -s "$HOME/.dotfiles/.bashrc" "$HOME/.bashrc"
+ln -s "$HOME/.dotfiles/.bash_aliases" "$HOME/.bash_aliases"
+ln -s "$HOME/.dotfiles/.gitconfig" "$HOME/.gitconfig"
