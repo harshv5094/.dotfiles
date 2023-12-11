8c807cd762fb072722eca52284961a5d561dfc80
harshv5094 (Local User) <harshv5094@gmail.com>
2023-12-11 17:44:08 +0530

    fix(sh): Fixing `dotfiles` script folder alignment

--- .scripts/dotfiles.sh
+++ .scripts/dotfiles.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-ln -s "$HOME/.dotfiles/.bashrc" "$HOME/.bashrc"
-ln -s "$HOME/.dotfiles/.bash_aliases" "$HOME/.bash_aliases"
-ln -s "$HOME/.dotfiles/.gitconfig" "$HOME/.gitconfig"
+ln -s "$HOME/.dotfiles/dot/.bashrc" "$HOME/.bashrc"
+ln -s "$HOME/.dotfiles/dot/.bash_aliases" "$HOME/.bash_aliases"
+ln -s "$HOME/.dotfiles/dot/.gitconfig" "$HOME/.gitconfig"
