da75fe89af83d708c50bdbb7d4d2282e9974c91b
harshv5094 (Local User) <harshv5094@gmail.com>
2024-01-01 18:28:07 +0530

    delete: dotfiles.sh

--- .scripts/dotfiles.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-ln -s "$HOME/.dotfiles/dot/.bashrc" "$HOME/.bashrc"
-ln -s "$HOME/.dotfiles/dot/.bash_aliases" "$HOME/.bash_aliases"
-ln -s "$HOME/.dotfiles/dot/.gitconfig" "$HOME/.gitconfig"
