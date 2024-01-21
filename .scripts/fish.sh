9c6b2716bb1d212c756bef1ff756c658ce5e3e3b
harshv5094 (Local User) <harshv5094@gmail.com>
2024-01-21 14:15:31 +0530

    chore: removing tide

--- .scripts/fish.sh
+++ .scripts/fish.sh
@@ -9,20 +9,14 @@ ln -s "$BASE_DIR/config-windows.fish" "$CONFIG_PATH"
 ln -s "$BASE_DIR/config-linux.fish" "$CONFIG_PATH"
 ln -s "$BASE_DIR/functions/fzf_change_directory.fish" "$CONFIG_PATH/functions/"
 ln -s "$BASE_DIR/functions/fish_user_key_bindings.fish" "$CONFIG_PATH/functions/"
-ln -s "$BASE_DIR/conf.d/tide.fish" "$CONFIG_PATH/conf.d/"
 
 echo "Installing z"
 fisher install jethrokuan/z
-
-# Mind the compatibility: https://github.com/IlanCosman/tide/wiki/Fish-version-compatibility
-echo "Installing a theme for fish"
-fisher install IlanCosman/tide
 
 echo "Installing edc bazz"
 fisher install edc/bass
 
 echo "Installign ssh-agent"
-echo installing ssh-agent
 fisher install danhper/fish-ssh-agent
 
 echo "Installing nvm"
