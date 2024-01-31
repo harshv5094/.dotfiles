50611640b6dba509deee6a74441771f8ef77b783
harshv5094 (Local User) <harshv5094@gmail.com>
2024-01-31 13:07:18 +0530

    feat(nvim/ui): Designing popup_menu
    This menu is for cmdline when it is in the center of the screen

--- .config/nvim/lua/plugins/ui.lua
+++ .config/nvim/lua/plugins/ui.lua
@@ -4,9 +4,27 @@ return {
 		"folke/noice.nvim",
 		config = function()
 			require("noice").setup({
-				view = {
+				views = {
 					cmdline = {
-						view = "cmdline",
+						view = "cmdline_popup",
+					},
+					popupmenu = {
+						relative = "editor",
+						position = {
+							row = 8,
+							col = "50%",
+						},
+						size = {
+							width = 60,
+							height = 10,
+						},
+						border = {
+							style = "rounded",
+							padding = { 0, 1 },
+						},
+						win_options = {
+							winhighlight = { Normal = "Normal", FloatBorder = "DiagnosticInfo" },
+						},
 					},
 				},
 			})
