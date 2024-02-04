df4d25cb0b2a0d145678de9c8b8a6abcc1ce192d
harshv5094 (Local User) <harshv5094@gmail.com>
2024-02-04 14:38:10 +0530

    feat: Modifying and renaming script

--- .scripts/config.sh
+++ .scripts/config.sh
@@ -3,3 +3,7 @@
 ln -s "$HOME/.dotfiles/.config/nvim" "$HOME/.config/"
 ln -s "$HOME/.dotfiles/.config/tmux" "$HOME/.config/"
 ln -s "$HOME/.dotfiles/.config/themes" "$HOME/.config/"
+
+ln -s "$HOME/.dotfiles/dot/.bashrc" "$HOME/.bashrc"
+ln -s "$HOME/.dotfiles/dot/.bash_aliases" "$HOME/.bash_aliases"
+ln -s "$HOME/.dotfiles/dot/.gitconfig" "$HOME/.gitconfig"
