2423b0e0d426526d252edea1bb53af772651261b
harshv5094 (Local User) <harshv5094@gmail.com>
2024-02-29 11:45:20 +0530

    feat(nvim): Enabling scss support

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -97,7 +97,7 @@ return {
 						grouped = true,
 						previewer = false,
 						initial_mode = "normal",
-						layout_config = { height = 30 },
+						layout_config = { height = 25 },
 					})
 				end,
 				desc = "Telescope -> File Browser",
--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -7,6 +7,7 @@ return {
 			ensure_installed = {
 				"cmake",
 				"python",
+				"scss",
 				"cpp",
 				"css",
 				"fish",
