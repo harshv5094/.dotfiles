519efbb1f8bd50752e93bd9bb4fe770f611cdfc1
harshv5094 (Local User) <harshv5094@gmail.com>
2024-03-02 20:13:46 +0530

    feat(fish): Adding `nvm use` snippet to load on startup
    This script allows to load the node js profile on system startup

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -70,3 +70,8 @@ set --export PATH $BUN_INSTALL/bin $PATH
 # bun
 set --export BUN_INSTALL "$HOME/.bun"
 set --export PATH $BUN_INSTALL/bin $PATH
+
+# Loads nvm while loading fish shell config
+if type -q nvm
+    nvm -s use
+end
