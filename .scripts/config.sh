2d84aa26bd34f7cfed4193f3aa7db14991c6b73d
harshv5094 (Local User) <harshv5094@gmail.com>
2024-03-08 19:51:35 +0530

    feat: Changing theme Folder

--- .scripts/config.sh
+++ .scripts/config.sh
@@ -2,7 +2,7 @@
 
 ln -s "$HOME/.dotfiles/.config/nvim" "$HOME/.config/"
 ln -s "$HOME/.dotfiles/.config/tmux" "$HOME/.config/"
-ln -s "$HOME/.dotfiles/.config/themes" "$HOME/.config/"
+ln -s "$HOME/.dotfiles/.config/starship" "$HOME/.config/"
 
 ln -s "$HOME/.dotfiles/dot/.bashrc" "$HOME/.bashrc"
 ln -s "$HOME/.dotfiles/dot/.bash_aliases" "$HOME/.bash_aliases"
