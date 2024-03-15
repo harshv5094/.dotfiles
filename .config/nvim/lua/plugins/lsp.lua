7d2763410ee865dbb58687615733e80d70133b09
harshv5094 (Local User) <harshv5094@gmail.com>
2024-03-15 21:05:17 +0530

    fix(nvim): disable reuse_win option in Telescope

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -7,7 +7,7 @@ return {
 				"stylua",
 				"taplo",
 				"pyright",
-        "powershell-editorial-service",
+				"powershell-editorial-service",
 				"selene",
 				"luacheck",
 				"shellcheck",
@@ -23,13 +23,25 @@ return {
 	-- lsp servers
 	{
 		"neovim/nvim-lspconfig",
+		init = function()
+			local keys = require("lazyvim.plugins.lsp.keymaps").get()
+			keys[#keys + 1] = {
+				"gd",
+				function()
+					-- Do Not Reuse Window
+					require("telescope.builtin").lsp_definitions({ reuse_win = false })
+				end,
+				desc = "Goto Definition",
+				has = "definition",
+			}
+		end,
 		opts = {
 			inlay_hints = { enabled = false },
 			---@type lspconfig.options
 			servers = {
 				cssls = {},
 				taplo = {},
-        powershell_es = {},
+				powershell_es = {},
 				pyright = {
 					cmd = { "pyright-langserver", "--stdio" },
 					settings = {
