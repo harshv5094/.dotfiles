4b3588a200c04e3c75be940457e876eb357d4529
harshv5094 (Local User) <harshv5094@gmail.com>
2024-03-15 20:53:10 +0530

    feat(nvim): Reverting back the setup

---
+++ .config/nvim/lua/plugins/editor.lua
@@ -0,0 +1,138 @@
+return {
+	-- Lazygit.nvim
+
+	{
+		"kdheepak/lazygit.nvim",
+		event = "VeryLazy",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+		},
+	},
+
+	-- Flash.nvim
+	{
+		enabled = false,
+		"folke/flash.nvim",
+		---@type Flash.Config
+		opts = {
+			search = {
+				forward = true,
+				multi_window = false,
+				wrap = false,
+				incremental = true,
+			},
+		},
+	},
+
+	{
+		"s1n7ax/nvim-window-picker",
+		name = "window-picker",
+		event = "VeryLazy",
+		version = "2.*",
+		config = function()
+			require("window-picker").setup({
+				hint = "statusline-winbar",
+				selection_chars = "FJDKSLA;CMRUEIWOQP",
+				show_prompt = true,
+				highlights = {
+					statusline = {
+						focused = {
+							fg = "#ededed",
+							bg = "#e35e4f",
+							bold = true,
+						},
+						unfocused = {
+							fg = "#ededed",
+							bg = "#44cc41",
+							bold = true,
+						},
+					},
+					winbar = {
+						focused = {
+							fg = "#ededed",
+							bg = "#e35e4f",
+							bold = true,
+						},
+						unfocused = {
+							fg = "#ededed",
+							bg = "#44cc41",
+							bold = true,
+						},
+					},
+				},
+			})
+		end,
+	},
+
+	-- Todo Comments
+	{
+		"folke/todo-comments.nvim",
+		event = "BufReadPre",
+		--- @type TodoComments.Config
+		opts = {
+			signs = true,
+			merge_keywords = true, -- when true, custom keywords will be merged with the defaults
+			gui_style = {
+				fg = "NONE", -- The gui style to use for the fg highlight group.
+				bg = "BOLD", -- The gui style to use for the bg highlight group.
+			},
+			highlight = {
+				multiline = true, -- enable multine todo comments
+				multiline_pattern = "^.", -- lua pattern to match the next multiline from the start of the matched keyword
+				multiline_context = 10, -- extra lines that will be re-evaluated when changing a line
+				before = "", -- "fg" or "bg" or empty
+				keyword = "wide", -- "fg", "bg", "wide", "wide_bg", "wide_fg" or empty. (wide and wide_bg is the same as bg, but will also highlight surrounding characters, wide_fg acts accordingly but with fg)
+				after = "fg", -- "fg" or "bg" or empty
+				pattern = [[.*<(KEYWORDS)\s*:]], -- pattern or table of patterns, used for highlighting (vim regex)
+				comments_only = true, -- uses treesitter to match keywords in comments only
+				max_line_len = 400, -- ignore lines longer than this
+				exclude = {}, -- list of file types to exclude highlighting
+			},
+			colors = {
+				error = { "DiagnosticError", "ErrorMsg", "#DC2626" },
+				warning = { "DiagnosticWarn", "WarningMsg", "#FBBF24" },
+				info = { "DiagnosticInfo", "#2563EB" },
+				hint = { "DiagnosticHint", "#10B981" },
+				default = { "Identifier", "#7C3AED" },
+				test = { "Identifier", "#FF00FF" },
+			},
+		},
+	},
+
+	-- Color Highlighter
+	{
+		"echasnovski/mini.hipatterns",
+		event = "BufReadPre",
+		opts = {
+			highlighters = {
+				hsl_color = {
+					pattern = "hsl%(%d+,? %d+%%?,? %d+%%?%)",
+					group = function(_, match)
+						local utils = require("solarized-osaka.hsl")
+						--- @type string, string, string
+						local nh, ns, nl = match:match("hsl%((%d+),? (%d+)%%,? (%d+)%%?%)")
+						--- @type number?, number?, number?
+						local h, s, l = tonumber(nh), tonumber(ns), tonumber(nl)
+						--- @type string
+						local hex_color = utils.hslToHex(h, s, l)
+						return MiniHipatterns.compute_hex_color_group(hex_color, "bg")
+					end,
+				},
+			},
+		},
+	},
+
+	-- Git
+	{
+		"dinhhuy258/git.nvim",
+		event = "BufReadPre",
+		opts = {
+			keymaps = {
+				-- Open blame window
+				blame = "<Leader>gb",
+				-- Open file/folder in git repository
+				browse = "<Leader>go",
+			},
+		},
+	},
+}
