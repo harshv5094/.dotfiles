edd226806afb6587793fc1f30cace2fe2a4fefc7
harshv5094 (Local User) <harshv5094@gmail.com>
2024-03-16 19:44:44 +0530

    feat(nvim): Enabling always show bufferline

--- .config/nvim/lua/plugins/ui.lua
+++ .config/nvim/lua/plugins/ui.lua
@@ -82,9 +82,10 @@ return {
 		opts = {
 			options = {
 				mode = "tabs",
-				-- separator_style = "slant",
 				show_buffer_close_icons = false,
+				always_show_bufferline = true,
 				show_close_icon = false,
+				diagnostics = "nvim_lsp",
 			},
 		},
 	},
