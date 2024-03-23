2914d8c49334128877d969153cb214bb2a9f6b93
harshv5094 (Local User) <harshv5094@gmail.com>
2024-03-23 15:15:19 +0530

    feat(fish): Adding script setup alias

--- .config/fish/alias.fish
+++ .config/fish/alias.fish
@@ -1,11 +1,18 @@
+alias setup "~/.dotfiles/.scripts/install.sh"
+
 if command -q eza
     alias ll "eza -lg --icons"
     alias ls "eza -g --icons"
     alias la "eza -lga --icons"
 end
 
+if command -q vim
+    alias setup_editor "vim ~/.dotfiles/.scripts/install.sh"
+end
+
 if command -q nvim
     alias vimedit "nvim ~/.dotfiles/.config/nvim/"
+    alias setup_editor "nvim ~/.dotfiles/.scripts/install.sh"
 end
 
 if command -q lazygit
