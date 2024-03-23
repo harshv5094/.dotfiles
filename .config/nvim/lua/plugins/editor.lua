d2e8754de891893b14d64b832dfc626574aa02c3
harshv5094 (Local User) <harshv5094@gmail.com>
2024-03-23 21:58:24 +0530

    feat(nvim): Combining code to one file

--- .config/nvim/lua/plugins/editor.lua
+++ .config/nvim/lua/plugins/editor.lua
@@ -64,6 +64,189 @@ return {
 		end,
 	},
 
+	--  Telescope
+	{
+		"telescope.nvim",
+		dependencies = {
+			{
+				"nvim-telescope/telescope-fzf-native.nvim",
+				build = "make",
+			},
+			"nvim-telescope/telescope-file-browser.nvim",
+		},
+		keys = {
+			{
+				";g",
+				function()
+					local builtin = require("telescope.builtin")
+					builtin.git_files()
+				end,
+				desc = "Telescope -> Git Files",
+			},
+			{
+				";F",
+				function()
+					local builtin = require("telescope.builtin")
+					builtin.grep_string({
+						search = vim.fn.input("Grep > "),
+					})
+				end,
+				desc = "Telescope -> Grep String",
+			},
+			{
+				"<leader>fP",
+				function()
+					require("telescope.builtin").find_files({
+						cwd = require("lazy.core.config").options.root,
+					})
+				end,
+				desc = "Telescope -> Find Plugin File",
+			},
+			{
+				";f",
+				function()
+					local builtin = require("telescope.builtin")
+					builtin.find_files({
+						no_ignore = false,
+						hidden = true,
+					})
+				end,
+				desc = "Telescope -> Find Files",
+			},
+			{
+				";r",
+				function()
+					local builtin = require("telescope.builtin")
+					builtin.live_grep({
+						additional_args = { "--hidden" },
+					})
+				end,
+				desc = "Telescope -> Live Grep",
+			},
+			{
+				"\\\\",
+				function()
+					local builtin = require("telescope.builtin")
+					builtin.buffers()
+				end,
+				desc = "Telescope -> Open Buffers",
+			},
+			{
+				";t",
+				function()
+					local builtin = require("telescope.builtin")
+					builtin.help_tags()
+				end,
+				desc = "Telescope -> Help Tags",
+			},
+			{
+				";;",
+				function()
+					local builtin = require("telescope.builtin")
+					builtin.resume()
+				end,
+				desc = "Telescope -> Resume",
+			},
+			{
+				";e",
+				function()
+					local builtin = require("telescope.builtin")
+					builtin.diagnostics()
+				end,
+				desc = "Telescope -> Diagnostics  ",
+			},
+			{
+				";s",
+				function()
+					local builtin = require("telescope.builtin")
+					builtin.treesitter()
+				end,
+				desc = "Telescope -> Treesitter",
+			},
+			{
+				"sf",
+				function()
+					local telescope = require("telescope")
+
+					local function telescope_buffer_dir()
+						return vim.fn.expand("%:p:h")
+					end
+
+					telescope.extensions.file_browser.file_browser({
+						path = "%:p:h",
+						select_buffer = true,
+						cwd = telescope_buffer_dir(),
+						respect_gitignore = false,
+						hidden = true,
+						grouped = true,
+						previewer = false,
+						initial_mode = "normal",
+						layout_config = { height = 25 },
+					})
+				end,
+				desc = "Telescope -> File Browser",
+			},
+		},
+		config = function(_, opts)
+			local telescope = require("telescope")
+			local actions = require("telescope.actions")
+			local fb_actions = telescope.extensions.file_browser.actions
+
+			opts.defaults = vim.tbl_deep_extend("force", opts.defaults, {
+				wrap_results = true,
+				layout_strategy = "horizontal",
+				layout_config = { prompt_position = "top" },
+				sorting_strategy = "ascending",
+				winblend = 0,
+				mappings = {
+					n = {},
+				},
+			})
+			opts.pickers = {
+				diagnostics = {
+					theme = "ivy",
+					initial_mode = "normal",
+					layout_config = {
+						preview_cutoff = 9999,
+					},
+				},
+			}
+			opts.extensions = {
+				file_browser = {
+					theme = "ivy",
+					-- disables netrw and use telescope-file-browser in its place
+					hijack_netrw = true,
+					mappings = {
+						-- your custom insert mode mappings
+						["n"] = {
+							-- your custom normal mode mappings
+							["N"] = fb_actions.create,
+							["h"] = fb_actions.goto_parent_dir,
+							["/"] = function()
+								vim.cmd("startinsert")
+							end,
+							["<C-u>"] = function(prompt_bufnr)
+								for i = 1, 10 do
+									actions.move_selection_previous(prompt_bufnr)
+								end
+							end,
+							["<C-d>"] = function(prompt_bufnr)
+								for i = 1, 10 do
+									actions.move_selection_next(prompt_bufnr)
+								end
+							end,
+							["<PageUp>"] = actions.preview_scrolling_up,
+							["<PageDown>"] = actions.preview_scrolling_down,
+						},
+					},
+				},
+			}
+			telescope.setup(opts)
+			require("telescope").load_extension("fzf")
+			require("telescope").load_extension("file_browser")
+		end,
+	},
+
 	-- Todo Comments
 	{
 		"folke/todo-comments.nvim",
@@ -135,4 +318,7 @@ return {
 			},
 		},
 	},
+
+	-- Tailwind Colorizer
+	{ "roobert/tailwindcss-colorizer-cmp.nvim", config = true },
 }
