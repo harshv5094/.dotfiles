0295b3193c24dd05014f0d539e987105b61826bc
harshv5094 (Local User) <harshv5094@gmail.com>
2024-03-24 00:36:07 +0530

    feat(nvim): Turning sidebar transparent and lualine bold

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -10,8 +10,9 @@ return {
 				styles = {
 					comments = { italic = true },
 					-- Background styles. Can be "dark", "transparent" or "normal"
-					sidebars = "dark", -- style for sidebars, see below
-					floats = "dark", -- style for floating windows			keywords = { italic = true },
+					sidebars = "transparent", -- style for sidebars, see below
+					floats = "dark", -- style for floating windows
+					lualine_bold = true, -- When `true`, section headers in the lualine theme will be bold
 				},
 			}
 		end,
