60bd88ab243a77114011e407a4fb02761cf5cffe
Harsh Vyapari (tumbleweed WSL) <harshv5094@gmail.com>
2024-03-28 11:16:51 +0530

    feat(fish,script): Improving fish script by adding installation status
    I've automated the whole procedure so it can be a less pain in the a**
    for me to understand what the f**k is happening right now

--- scripts/fish.sh
+++ scripts/fish.sh
@@ -2,17 +2,37 @@
 
 set CONFIG_PATH ~/.config/fish
 set BASE_DIR $HOME/.dotfiles/.config/fish
+
+if test -d "$CONFIG_PATH"
+  echo "Removing Existing Config Files for fish"
+  rm -rf "$CONFIG_PATH"
+  echo "Making Necessary Directories for fish"
+  mkdir ~/.config/fish
+  mkdir ~/.config/fish/functions/
+  mkdir ~/.config/fish/conf.d/
+end
+
+echo "Copying files to config path"
 ln -s "$BASE_DIR/config.fish" "$CONFIG_PATH"
 ln -s "$BASE_DIR/alias.fish" "$CONFIG_PATH"
 ln -s "$BASE_DIR/config-osx.fish" "$CONFIG_PATH"
 ln -s "$BASE_DIR/config-windows.fish" "$CONFIG_PATH"
 ln -s "$BASE_DIR/config-linux.fish" "$CONFIG_PATH"
-mkdir "$CONFIG_PATH/functions/"
+ln -s "$BASE_DIR/conf.d/tide.fish" "$CONFIG_PATH"
 ln -s "$BASE_DIR/functions/fzf_change_directory.fish" "$CONFIG_PATH/functions/"
 ln -s "$BASE_DIR/functions/fish_user_key_bindings.fish" "$CONFIG_PATH/functions/"
 
+
+# Check if fisher is installed
+if not functions -q fisher
+    echo "fisher is not installed. Installing fisher..."
 curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher
+    echo "fisher installed successfully."
+end
 
-cp -rf ~/.dotfiles/scripts/fish_plugins ~/.config/fish/
+# Copy fish_plugins files to the Fish directory
+cp -rf fish_plugins ~/.config/fish/
 
-fisher update
+# Install plugins using fisher
+fish -c "fisher update"
+
