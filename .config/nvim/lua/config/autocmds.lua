3e3063c6f4af773d2b2530db709f71c017ec80aa
harshv5094 (personal PC) <harshv5094@gmail.com>
2024-10-10 16:20:36 +0530

    Revert "feat(autocmds.lua): adding colors for cmdline and relative line"
    This reverts commit a6db071351613f988e2e4070f68dbf7de48ba94f.

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -4,20 +4,7 @@ vim.api.nvim_create_autocmd("InsertLeave", {
   command = "set nopaste",
 })
 
-local enter_group = { "BufWinEnter", "CmdlineEnter" }
-
 -- Changing cmdline color
-vim.api.nvim_create_autocmd(enter_group, {
-  pattern = "*",
+vim.api.nvim_create_autocmd({ "BufEnter", "CmdlineEnter", "VimEnter" }, {
   command = "hi MsgArea guifg=#fca7ea",
-})
--- Changing Number colors
-vim.api.nvim_create_autocmd(enter_group, {
-  pattern = "*",
-  command = "hi LineNrAbove guifg=#828bb8",
-})
-
-vim.api.nvim_create_autocmd(enter_group, {
-  pattern = "*",
-  command = "hi LineNrBelow guifg=#828bb8",
 })
