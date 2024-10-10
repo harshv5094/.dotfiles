33c0cd780062238fda473415a0382548fd9acdf5
harshv5094 (personal PC) <harshv5094@gmail.com>
2024-10-10 13:27:06 +0530

    feat(nvim): adding code context window

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -10,11 +10,29 @@ return {
         "lua",
         "markdown",
         "markdown_inline",
-        "python",
         "query",
         "regex",
         "vim",
       })
+    end,
+  },
+  {
+    "nvim-treesitter/nvim-treesitter-context",
+    opts = function()
+      local context = require("treesitter-context")
+      local opts = context.setup({
+        enable = true,
+        max_lines = 0,
+        min_window_height = 0,
+        line_numbers = true,
+        multiline_threshold = 20,
+        trim_scope = "outer",
+        mode = "cursor",
+        seperator = nil,
+        zindex = 10,
+        on_attach = nil,
+      })
+      return opts
     end,
   },
 }
