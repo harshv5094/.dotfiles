bb35dd8c43a5486f6acea964019deb1dfc9b25a3
harshv5094 (personal PC) <harshv5094@gmail.com>
2024-10-18 19:52:47 +0530

    chore(nvim): removing typescript setup

--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -28,7 +28,6 @@ require("lazy").setup({
 		{ import = "lazyvim.plugins.extras.lang.clangd" },
 		{ import = "lazyvim.plugins.extras.lang.ansible" },
 		{ import = "lazyvim.plugins.extras.lang.json" },
-		{ import = "lazyvim.plugins.extras.lang.typescript" },
 		{ import = "lazyvim.plugins.extras.lang.yaml" },
 		{ import = "lazyvim.plugins.extras.lang.toml" },
 
