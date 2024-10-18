9967d89b960a64e6deadcd84e820e32e5bbcda45
harshv5094 (personal PC) <harshv5094@gmail.com>
2024-10-18 19:45:27 +0530

    chore(nvim): removing rust setup

--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -29,7 +29,6 @@ require("lazy").setup({
 		{ import = "lazyvim.plugins.extras.lang.ansible" },
 		{ import = "lazyvim.plugins.extras.lang.json" },
 		{ import = "lazyvim.plugins.extras.lang.typescript" },
-		{ import = "lazyvim.plugins.extras.lang.rust" },
 		{ import = "lazyvim.plugins.extras.lang.yaml" },
 		{ import = "lazyvim.plugins.extras.lang.toml" },
 
