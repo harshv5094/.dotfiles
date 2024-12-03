fa6f46d2b7184fe3f911e47e0973ddddbc6d6337
harshv5094 <harshv5094@gmail.com>
2024-12-03 20:00:56 +0530

    feat(nvim/lazy.lua): enable typescript support

--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -35,6 +35,7 @@ require("lazy").setup({
 		{ import = "lazyvim.plugins.extras.lang.clangd" },
 		{ import = "lazyvim.plugins.extras.lang.python" },
 		{ import = "lazyvim.plugins.extras.lang.ansible" },
+		{ import = "lazyvim.plugins.extras.lang.typescript" },
 		{ import = "lazyvim.plugins.extras.lang.json" },
 		{ import = "lazyvim.plugins.extras.lang.yaml" },
 		{ import = "lazyvim.plugins.extras.lang.toml" },
