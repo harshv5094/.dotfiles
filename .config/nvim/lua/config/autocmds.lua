f8fca93de5fca603056013d1cf744b4a5415c83f
harshv5094 <harshv5094@gmail.com>
2024-12-16 17:13:52 +0530

    fix(nvim): fixing comment and numline color for transparent bg

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -12,3 +12,10 @@ vim.api.nvim_create_autocmd("FileType", {
 		vim.opt.conceallevel = 0
 	end,
 })
+
+-- Setting up relative line number and comment color
+local fgcolor = "#636da6"
+local bgcolor = "none"
+vim.api.nvim_set_hl(0, "LineNrAbove", { fg = fgcolor, bg = bgcolor })
+vim.api.nvim_set_hl(0, "Comment", { fg = fgcolor, bg = bgcolor })
+vim.api.nvim_set_hl(0, "LineNrBelow", { fg = fgcolor, bg = bgcolor })
