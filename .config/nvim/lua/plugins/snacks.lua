f02fd04aae79d7e7964b8c1702eb59ae8d1f80da
Harsh Vyapari <harshvy5094@proton.me>
2025-01-20 16:09:47 +0530

    feat(nvim/dashboard): add new section of git status and browse repo option

--- .config/nvim/lua/plugins/snacks.lua
+++ .config/nvim/lua/plugins/snacks.lua
@@ -16,7 +16,9 @@ return {
 
 			-- NOTE: Dashboard Keys
 			opts.dashboard.preset.keys = {
+				{ icon = " ", key = "f", desc = "Find File", action = ":lua Snacks.dashboard.pick('files')" },
 				{ icon = " ", key = "n", desc = "New File", action = ":ene | startinsert" },
+				{ icon = " ", key = "g", desc = "Find Text", action = ":lua Snacks.dashboard.pick('live_grep')" },
 				{ icon = " ", key = "r", desc = "Recent Files", action = ":lua Snacks.dashboard.pick('oldfiles')" },
 				{
 					icon = " ",
@@ -25,14 +27,38 @@ return {
 					action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})",
 				},
 				{ icon = " ", key = "s", desc = "Restore Session", section = "session" },
-				{ icon = "󰒲 ", key = "l", desc = "Lazy", action = ":Lazy" },
+				{ icon = "󰒲 ", key = "l", desc = "Lazy", action = ":Lazy", enabled = package.loaded.lazy ~= nil },
 				{ icon = " ", key = "q", desc = "Quit", action = ":qa" },
 			}
 
 			-- NOTE: Dashboard Sections
 			opts.dashboard.sections = {
 				{ section = "header" },
-				{ section = "keys", gap = 1, padding = 1 },
+				{ icon = " ", title = "Keymaps", section = "keys", indent = 2, padding = 1 },
+				{
+					icon = " ",
+					desc = "Browse Repo",
+					key = "b",
+					enabled = function()
+						return Snacks.git.get_root() ~= nil
+					end,
+					action = function()
+						Snacks.gitbrowse()
+					end,
+				},
+				{
+					icon = " ",
+					title = "Git Status",
+					section = "terminal",
+					enabled = function()
+						return Snacks.git.get_root() ~= nil
+					end,
+					cmd = "git status --short --branch --renames",
+					height = 5,
+					padding = 1,
+					ttl = 5 * 60,
+					indent = 3,
+				},
 				{ section = "startup" },
 			}
 		end,
