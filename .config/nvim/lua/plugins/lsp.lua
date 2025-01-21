6028575a465344a9afd479fa76da663d3c48b7c7
Harsh Vyapari <harshvy5094@proton.me>
2025-01-21 18:27:31 +0530

    feat(nvim/lsp): add emoji completion support for blink.cmp
    This allows me to use emoji's in blink

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -12,6 +12,29 @@ return {
 		},
 	},
 
+	-- Add emoji support for blink.cmp
+	{
+		"saghen/blink.cmp",
+		dependencies = {
+			"moyiz/blink-emoji.nvim",
+		},
+		opts = {
+			sources = {
+				default = {
+					"emoji",
+				},
+				providers = {
+					emoji = {
+						module = "blink-emoji",
+						name = "Emoji",
+						score_offset = 15, -- Tune by preference
+						opts = { insert = true }, -- Insert emoji (default) or complete its name
+					},
+				},
+			},
+		},
+	},
+
 	-- Configuring LSP
 	{
 		"neovim/nvim-lspconfig",
