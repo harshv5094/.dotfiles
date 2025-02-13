7fc25f410b030a583176acd7926ef6ecbdebc3ed
Harsh Vyapari (harshv5094) <harshvy5094@proton.me>
2025-02-13 14:32:50 +0530

    chore(fish.sh): remove useless apps

--- .scripts/fish.sh
+++ .scripts/fish.sh
@@ -4,10 +4,10 @@
 info_msg "** Install fish shell and required cli tools"
 package_manager=$(command -v pacman || true)
 if [ -n "${package_manager}" ]; then
-  "${ESCALATION_TOOL}" "${package_manager}" -S --needed --noconfirm fish vdpauinfo flatpak tree git github-cli git-lfs git-filter-repo \
+  "${ESCALATION_TOOL}" "${package_manager}" -S --needed --noconfirm fish flatpak tree git github-cli git-lfs git-filter-repo \
     wget curl zip unzip peco ripgrep fzf sox bat eza lazygit btop fd \
     zoxide yt-dlp xsel fastfetch openssh tldr yazi \
-    trash-cli fwupd usbutils cronie imagemagick man-db bind speech-dispatcher \
+    trash-cli usbutils cronie imagemagick man-db vdpauinfo \
     starship bash-completion neovim newsboat go rust base-devel make cmake ninja luarocks hugo
 fi
 
